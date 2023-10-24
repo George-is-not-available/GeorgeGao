3a22a39d03ff3c4e6a7041e39457c9d8b45326a1
GeorgeGao <georgegao@moonshotacademy.cn>
2023-10-24 12:03:39 +0800

    Changes

--- Unit1/PA/井字棋变化玩法.py
+++ Unit1/PA/井字棋变化玩法.py
@@ -4,6 +4,7 @@ import random
 
 moves = []
 
+
 def get_valid_bet_input(player_name):
     while True:
         try:
@@ -12,11 +13,12 @@ def get_valid_bet_input(player_name):
         except ValueError:
             print("\033[91mError Invalid input. \033[95mPlease enter a valid number for the bet.\033[0m")
 
-def get_player_input(current_player, moves, board, move_count):
+
+def get_player_input(current_player, move, board, move_count):
     while True:
         try:
             if move_count >= 7:
-                removed_move = moves.pop(0)
+                removed_move = move.pop(0)
                 board[removed_move] = " "  # 清除第一个棋子
             move_ = int(input(f"Player {current_player}, choose a position (1-9) to play: "))
             if 1 <= move_ <= 9:
@@ -46,6 +48,7 @@ def get_player_input(current_player, moves, board, move_count):
             print("\033[91m"
                   "What's problem with you?\033[0m")
             move_count += 1  # 更新移动计数
+
 
 def main():
     print("DownLoading    (Expect to download    170.52MB)")
@@ -88,6 +91,7 @@ def main():
         # ... 检查胜利和平局的代码 ...
 
         current_player = "X" if current_player == "O" else "O"
+
 
 if __name__ == "__main__":
     main()
