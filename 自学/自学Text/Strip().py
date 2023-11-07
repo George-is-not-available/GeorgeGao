0be5e7c1dc1eec8e195f42010e3d71a9d4565bb2
GeorgeGao <georgegao@moonshotacademy.cn>
2023-11-07 11:36:35 +0800

    自学

---
+++ 自学/自学Text/Strip().py
@@ -0,0 +1,12 @@
+"""
+功能：用于删除一段程序语句末尾或开头的换行符。
+
+例如：
+
+string = "this is a book\n"
+print(string.strip())
+输出结果为：this is a book
+"""
+
+string = "\nThis is a book\n"
+print(string.strip())
---
+++ 自学/自学Text/startswith.py
@@ -0,0 +1,14 @@
+"""
+功能：用于检验在一段程序语句中，是否以特定元素（元素可以是单词、字母或数字）开头。
+
+例如：
+
+string = "this is a book"
+print(string.endswith('book'))
+
+输出结果为：True
+
+"""
+
+string = "this is a book"
+print(string.endswith('book'))
