312e93ce09095ccb264d1ad30bc6358144e2bab3
GeorgeGao <georgegao@moonshotacademy.cn>
2023-11-07 12:07:14 +0800

    自学

---
+++ 自学/在家/replsce.py
@@ -0,0 +1,13 @@
+"""
+功能：对一段程序语句中的构成元素进行替换。
+
+例如：
+
+string = "Python is good"
+print(string.replace('Python','java'))
+
+ 输出结果为：java is good
+"""
+
+string = "Python is good"
+print(string.replace('Python','java'))
