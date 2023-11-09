b0e4d235a3487206960a74d85d3e3f7dde90997e
GeorgeGao <georgegao@moonshotacademy.cn>
2023-11-09 08:45:28 +0800

    自学

---
+++ Unit2/Chapter1/2.1.py
@@ -0,0 +1,13 @@
+"""
+name,attributes,methods
+"""
+class School:
+    name = ''
+    name_of_student = 0
+
+    def drop(self):
+        pass
+
+    msa = School()
+    print(msa.name)
+    msa.drop()
