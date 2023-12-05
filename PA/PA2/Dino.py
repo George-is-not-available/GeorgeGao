3bf463789f0024e13bc024298159f947bed6ed29
GeorgeGao <GeorgeGao@moonshot.cn>
2023-12-05 10:46:42 +0800

    Changes

---
+++ PA/PA2/Dino.py
@@ -0,0 +1,30 @@
+class Scene:
+    class Ground:
+        pass
+
+    class Moon:
+        pass
+
+    class Cloud:
+        pass
+
+    class Star:
+        pass
+
+    class Scoreboard:
+        pass
+
+    class Endinglcon:
+        pass
+
+
+class Obstacle:
+    class Cactus:
+        pass
+
+    class Pterodactyl:
+        pass
+
+    class Dinosaur:
+        class Cactus:
+            pass
